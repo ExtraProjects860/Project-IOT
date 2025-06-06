341b62828bf30bb75af215d1762f480fdcfeb3c3
Davi <oversouls11@gmail.com>
2025-06-05 23:30:14 -0300

    Removendo Arquivos desnecessários

--- app/dependencies/__init__.py
+++
@@ -1,1 +0,0 @@
-from app.dependencies.database_dependencies import DBSessionDep
--- app/dependencies/database_dependencies.py
+++
@@ -1,6 +0,0 @@
-from typing import Annotated
-from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi import Depends
-from app import config
-
-DBSessionDep = Annotated[AsyncSession, Depends(config.get_db_session)]
